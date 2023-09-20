f57cc5e430ea33122999308af1f09d73c284590b
umarhasan1234 <umarhasan733@gmail.com>
2023-09-20 07:34:06 +0530

    created

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -3,7 +3,6 @@ package controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +18,7 @@ import in.demo.service.UserService;
 
 @RestController
 @RequestMapping("/vpt/user")
-public class UserController {
+public class UserController<Umar> {
 	
 	@Autowired
 	private UserService userService;
@@ -41,7 +40,7 @@ public class UserController {
 		return "update successfull";
 	}
 	
-	Umar u=new Umar();
+	
 	
 
 }
